e7f45712e0e49f5721045fd887c36e305d925d57
JuliaLes <JCLes92@gmail.com>
2016-04-24 17:22:58 -0400

    Hoemwork attempt 1

---
+++ Farenheit_to_Celsius.rb
@@ -0,0 +1,8 @@
+# Converting Farenheit to Celsius 
+
+def farenheit_to_Celsius (f)
+    
+    c = ((f - 32.0) * 5.0/9.0).round(1)   
+    puts "#{f} degrees Farenheit is equal to #{c} degrees Celsius."
+    
+end
